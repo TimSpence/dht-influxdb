655899489c5ba455d2fa2d985eb9308c2b96ad68
Tim Spence <yogi.wan.kenobi@gmail.com>
2017-06-17 23:25:24 +0000

    changed pin and model to constants

--- bin/logger.rb
+++ bin/logger.rb
@@ -3,10 +3,14 @@
 require 'influxdb'
 require 'dht-sensor-ffi'
 
+VERBOSE = false
+DHT_MODEL = 22  #models are dht-11 and dht-22
+GPIO_PIN = 4
+
 influxdb = InfluxDB::Client.new("logger")
 
 while true
-  val = DhtSensor.read(4,22)
+  val = DhtSensor.read(GPIO_PIN,DHT_MODEL)
   es = 0.6108 * Math.exp(17.27 * val.temp / (val.temp + 237.3))
   ea = val.humidity / 100 * es
   vpd = (ea - es).abs
@@ -26,7 +30,7 @@ while true
 
   influxdb.write_point("conditions", data)
 
-  puts "wrote: #{data}"
+  puts "wrote: #{data}" if VERBOSE
   sleep(2)
 end
 
