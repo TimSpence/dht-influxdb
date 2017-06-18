1d0444d2e986e7a872cc9b08131cbbe1e5a86af8
Tim Spence <yogi.wan.kenobi@gmail.com>
2017-06-18 00:13:32 +0000

    plugin compatible binary

---
+++ bin/collect-one.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+require 'json'
+require 'dht-sensor-ffi'
+
+VERBOSE = false
+DHT_MODEL = 22  #models are dht-11 and dht-22
+GPIO_PIN = 4
+
+  val = DhtSensor.read(GPIO_PIN,DHT_MODEL)
+  es = 0.6108 * Math.exp(17.27 * val.temp / (val.temp + 237.3))
+  ea = val.humidity / 100 * es
+  vpd = (ea - es).abs
+  data = {
+    values: { temperature: val.temp_f,
+      humidity: val.humidity,
+      es: es,
+      ea: ea,
+      vpd: vpd },
+    timestamp: Time.now.to_i,
+    series: 'conditions',
+    tags: { 
+      valid_humidity: val.humidity > 10.0 && val.humidity < 95.0 ? true : false,
+      valid_temp: val.temp_f > 20 && val.temp_f < 125 ? true : false
+    }
+  }
+
+  puts data.to_json
+
+  puts "wrote: #{data}" if VERBOSE
