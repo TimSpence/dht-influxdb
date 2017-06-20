8c4d232c89cc493ae4f0185dccf1be6c7561007d
Tim Spence <yogi.wan.kenobi@gmail.com>
2017-06-20 03:01:53 +0000

    DRY'd up validation

--- bin/collect.rb
+++ bin/collect.rb
@@ -24,8 +24,8 @@ humidity_filter_a = KalmanFilter.new(
 while true
   val = DhtSensor.read(GPIO_PIN,DHT_MODEL)
   vpd = Ambient.VPD(val.temp, val.humidity)
-  temp_filter_a.measurement = val.temp_f
-  humidity_filter_a.measurement = val.humidity
+  temp_filter_a.measurement = val.temp_f if Ambient.is_valid_temp?(val.temp)
+  humidity_filter_a.measurement = val.humidity if Ambient.is_valid_rh?(val.humidity)
 
   data = {
     values: { temperature: val.temp_f,
@@ -36,8 +36,8 @@ while true
     timestamp: Time.now.to_i,
     series: 'conditions',
     tags: { 
-      valid_humidity: val.humidity > 10.0 && val.humidity < 95.0 ? true : false,
-      valid_temp: val.temp_f > 20 && val.temp_f < 125 ? true : false
+      valid_humidity: Ambient.is_valid_rh?(val.humidity),
+      valid_temp: Ambient.is_valid_temp?(val.temp)
     }
   }
 
--- lib/ambient.rb
+++ lib/ambient.rb
@@ -5,4 +5,12 @@ class Ambient
     ea = rh / 100.0 * es
     (ea - es).abs
   end
+
+  def self.is_valid_temp?(temp)
+    temp > -7.0 && temp < 51
+  end
+
+  def self.is_valid_rh?(rh)
+    rh > 10.0 && rh < 95.0
+  end
 end
