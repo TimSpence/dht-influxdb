b3351f3b42fb24edbb215dc9fb0578f3abb99bbf
Tim Spence <yogi.wan.kenobi@gmail.com>
2017-06-22 18:22:45 -0700

    add specs for valid ranges

--- spec/temperature_spec.rb
+++ spec/temperature_spec.rb
@@ -22,6 +22,15 @@ RSpec.describe(TemperatureMeasurement, type: :temperaturemeasurement) do
     t = TemperatureMeasurement.new(0.0, TemperatureScale::Celsius)
     expect(t.scale).to eq(TemperatureScale::Celsius)
   end
+  it "accepts a scale of Fahrenheit" do
+    t = TemperatureMeasurement.new(0.0, TemperatureScale::Fahrenheit)
+    expect(t.scale).to eq(TemperatureScale::Fahrenheit)
+  end
+  it "don't do Kelvin" do
+    expect {
+      t = TemperatureMeasurement.new(0.0, TemperatureScale::Kelvin)
+    }.to raise_error(NameError)
+  end
   it "converts celsius to fahrenheit" do
     t = TemperatureMeasurement.new(0.0, TemperatureScale::Celsius)
     expect(t.to_fahrenheit).to eq(32.0)
@@ -29,5 +38,17 @@ RSpec.describe(TemperatureMeasurement, type: :temperaturemeasurement) do
   it "converts fahrenheit to celsius" do
     t = TemperatureMeasurement.new(32.0, TemperatureScale::Fahrenheit)
     expect(t.to_celsius).to eq(0.0)
+  end
+  it "reports valid if temperature in valid range" do
+    t = TemperatureMeasurement.new(75.0, TemperatureScale::Fahrenheit)
+    expect(t.is_valid?).to be(true)
+  end
+  it "reports not valid if temperature below valid range" do
+    t = TemperatureMeasurement.new(19.0, TemperatureScale::Fahrenheit)
+    expect(t.is_valid?).to be(false)
+  end
+  it "reports not valid if temperature above valid range" do
+    t = TemperatureMeasurement.new(126.0, TemperatureScale::Fahrenheit)
+    expect(t.is_valid?).to be(false)
   end
 end
