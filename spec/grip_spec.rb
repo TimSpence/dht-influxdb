479ca5038a09f449eb4a8e05c2f616f95ac8f3d3
Tim Spence <yogi.wan.kenobi@gmail.com>
2017-06-24 21:48:19 -0700

    fixed broken test

--- spec/grip_spec.rb
+++ spec/grip_spec.rb
@@ -27,8 +27,8 @@ RSpec.describe(Grip, type: :grip) do
   it "outputs json" do
     t = TemperatureMeasurement.new(25.0)
     g = Grip.new("weather", [ t ])
-    j = JSON.parse(g.as_json)
-    expect(j['series']).to eq("weather")
+    j = g.as_json
+    expect(j.class).to be(Hash)
   end
   it "outputs line protocol" do
     t = TemperatureMeasurement.new(25.0)
